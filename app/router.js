844dedc9a136fcc4859e97d1ca4e93cc5bfe9b95
Mike Zornek <mike@clickablebliss.com>
2016-10-06 18:55:49 -0400

    No longer want to use resource for routes.

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.resource('pages', function () {
+  this.route('pages', function () {
     this.route('new');
   });
 });
